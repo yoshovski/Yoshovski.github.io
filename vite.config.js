ce1abcce838251085d9584608b23093ab00d4148
Stefan Yoshovski <stefan.yoshovski@gmail.com>
2025-03-10 00:31:15 +0100

    refactor: configs

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // Imposta la directory principale come root
+  build: {
+    outDir: 'dist', // Directory di output per i file di build
+  },
+});
